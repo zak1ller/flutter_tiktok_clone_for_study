c6254457bd4ae45690a55ba2c0386d8865b482f3
zak1ller <zak1ller.kr@gmail.com>
2023-10-31 13:41:13 +0900

    4.8 Birthday Screen

--- lib/featurs/authentication/birthday_view.dart
+++ lib/featurs/authentication/birthday_view.dart
@@ -1,10 +1,129 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/featurs/authentication/email_view.dart';
+import 'package:tiktok_clone/featurs/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/featurs/onboarding/interests_view.dart';
 
-class BirthdayView extends StatelessWidget {
+class BirthdayView extends StatefulWidget {
   const BirthdayView({super.key});
 
   @override
+  State<BirthdayView> createState() => _BirthdayViewState();
+}
+
+class _BirthdayViewState extends State<BirthdayView> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initialDate = DateTime.now().subtract(
+    const Duration(days: 365 * 12),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose();
+    super.dispose();
+  }
+
+  void _onScaffholdTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onNextTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestsView(),
+      ),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(" ").first;
+    _birthdayController.value = TextEditingValue(
+      text: textDate,
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GestureDetector(
+      onTap: _onScaffholdTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Sign up",
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size32,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              const Text(
+                "When's your birthday?",
+                style: TextStyle(
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Gaps.v8,
+              const Text(
+                "You birthday won't be shown publicy.",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Gaps.v16,
+              TextField(
+                enabled: false,
+                controller: _birthdayController,
+                cursorColor: Theme.of(context).primaryColor,
+                onEditingComplete: _onNextTap,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+              ),
+              Gaps.v16,
+              FormButton(
+                onTap: _onNextTap,
+                disabled: false,
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 300,
+            child: CupertinoDatePicker(
+              maximumDate: initialDate,
+              initialDateTime: initialDate,
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: _setTextFieldDate,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/featurs/onboarding/interests_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InterestsView extends StatelessWidget {
+  const InterestsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
