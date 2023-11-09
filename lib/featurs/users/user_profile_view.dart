2aa9850998620b82d2af5382eb777736b023a8c5
= <=>
2023-11-09 16:58:49 +0900

    12.2 SliverAppBar

--- lib/featurs/users/user_profile_view.dart
+++ lib/featurs/users/user_profile_view.dart
@@ -10,11 +10,19 @@ class UserProfileView extends StatefulWidget {
 class _UserProfileViewState extends State<UserProfileView> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
-        SliverAppBar(
+        const SliverAppBar(
           title: Text("Profile"),
         ),
+        SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => Container(
+                color: Colors.black.withOpacity(0.5),
+                child: Center(child: Text("Item $index")),
+              ),
+            ),
+            itemExtent: 100),
       ],
     );
   }
