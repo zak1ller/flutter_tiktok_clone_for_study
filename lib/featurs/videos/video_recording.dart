0f4ab4860b8427ca66845b8c65d59611541797d4
zak1ller <zak1ller.kr@gmail.com>
2023-11-14 21:30:18 +0900

    19.1 Installation

---
+++ lib/featurs/videos/video_recording.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoRecording extends StatefulWidget {
+  const VideoRecording({super.key});
+
+  @override
+  State<VideoRecording> createState() => _VideoRecordingState();
+}
+
+class _VideoRecordingState extends State<VideoRecording> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
